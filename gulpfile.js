2c508ab6f1aed2ccc57f5f8621a819908a2cfee4
Earl Debicki <bartosz.debicki@uxpin.com>
2017-07-04 00:35:38 +0200

    new js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,87 @@
 // require gulp
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minify = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    symlink = require('gulp-symlink'),
-    del = require('del'),
-    public = './public/',
-    lessPackageFile = './src/less/application.less',
-    lessFiles = './src/less/**/*.less',
-    lessFileDest = './public/css/',
-    jsFilesList = [],
-    jsFiles = './src/js/**/*.js',
-    jsFileDest = './public/js/',
-    jspackageFile = 'application.js',
-    symlinkSrcList = ['./src/fonts',
-                    './src/images',
-                    './src/views/index.html'],
-    symlinkDestList = ['./public/fonts',
-                     './public/images',
-                     './public/index.html'];
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var babel = require('babelify');
+var symlink = require('gulp-symlink');
+var del = require('del');
+
+var publicFolder = './public/';
+var srcFolder = './src/';
+var cssDestFolder = publicFolder + 'css/';
+var lessSrcPackageFiles = srcFolder + 'less/*.less';
+var lessSrcFiles = srcFolder + 'less/**/*.less';
+var jsPackageFile = 'application.js';
+var jsDestFolder = publicFolder + 'js/';
+var jsSrcFiles = srcFolder + 'js/**/*.js';
+var jsSrcPackageFile = srcFolder + 'js/' + jsPackageFile;
+var symlinkSrcList = [srcFolder + 'fonts',
+                      srcFolder + 'images',
+                      srcFolder + 'views/index.html'];
+var symlinkDestList = [publicFolder + 'fonts',
+                       publicFolder + 'images',
+                       publicFolder + 'index.html'];
+
+var bundler = browserify(jsSrcPackageFile, { debug: true })
+                .transform(babel.configure({
+                    presets: ["es2015"],
+                    plugins: ["transform-decorators-legacy"]
+                }));
 
 // remove public folder
 gulp.task('clean:public', function () {
-    return del(public);
+    return del(publicFolder);
 });
 
 // build css file task
 gulp.task('less', function() {
-    return gulp.src(lessPackageFile)
+    return gulp.src(lessSrcPackageFiles)
                .pipe(less())
                .pipe(autoprefixer({ browsers: ['last 2 versions', 'ie >= 10'] }))
-               .pipe(gulp.dest(lessFileDest));
+               .pipe(gulp.dest(cssDestFolder));
 });
 
 // build minified css file task
 gulp.task('less:minify', function() {
-    return gulp.src(lessPackageFile)
+    return gulp.src(lessSrcPackageFiles)
                .pipe(less())
                .pipe(autoprefixer({ browsers: ['last 2 versions', 'ie >= 10'] }))
                .pipe(minify())
-               .pipe(gulp.dest(lessFileDest));
+               .pipe(gulp.dest(cssDestFolder));
 });
 
 // build js file task
 gulp.task('js', function() {
-    return gulp.src(jsFilesList)
-               .pipe(gulp.dest(jsFileDest));
+    bundler.bundle()
+           .on('error', function(error) { console.log(error.toString()); this.emit('end'); })
+           .pipe(source(jsPackageFile))
+           .pipe(buffer())
+           .pipe(gulp.dest(jsDestFolder));
 });
 
 // build minified js file task
 gulp.task('js:minify', function() {
-    return gulp.src(jsFilesList)
-               .pipe(uglify())
-               .pipe(gulp.dest(jsFileDest));
+    bundler.bundle()
+           .on('error', function(error) { console.log(error.toString()); this.emit('end'); })
+           .pipe(source(jsPackageFile))
+           .pipe(buffer())
+           .pipe(uglify())
+           .pipe(gulp.dest(jsDestFolder));
 });
 
 // css watch task
 gulp.task('watch:less', function() {
-    return gulp.watch(lessFiles, ['less'])
+    return gulp.watch(lessSrcFiles, ['less'])
 });
 
 // js watch task
 gulp.task('watch:js', function() {
-    return gulp.watch(jsFiles, ['js'])
+    return gulp.watch(jsSrcFiles, ['js'])
 });
 
 // global watch task
